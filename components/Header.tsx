c5a33c2ef23819e25965a29e0b9fe5be59a7ffc6
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-09 01:07:13 +0300

    write adaptive points for header-decore

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,14 +9,19 @@ import { ArrowToBottom } from '@/assets/icons';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center mt-4 md:mt-14 after:content-[''] after:bg-[url('../assets/header-decore.svg')] after:block after:h-[700px] after:w-[700px] after:absolute after:top-0 after:right-0">
+    <header className="flex justify-between items-center mt-4 md:mt-14 after:content-[''] after:bg-[url('../assets/header-decore.svg')] after:block after:w-[300px] after:h-[550px] sm:after:w-[550px] sm:after:h-[700px] md:after:w-[700px] after:absolute after:top-0 after:right-0">
       <Link className='z-40' href={'/'} aria-label={'home'}>
-        <Image src={logo} alt={'logo'} priority />
+        <Image
+          className='h-7 w-auto md:h-10'
+          src={logo}
+          alt={'logo'}
+          priority
+        />
       </Link>
 
       <div className='hidden md:flex items-center z-50'>
         <nav>
-          <ul className='flex gap-14'>
+          <ul className='flex gap-7 lg:gap-14'>
             {navLinks.map((link, idx) => (
               <li key={idx}>
                 <NavLink
@@ -30,13 +35,17 @@ const Header = () => {
         </nav>
 
         <Link
-          className='border border-dark-blue rounded-md py-2 px-5 ml-14 hover:bg-[#F1A501]  focus:bg-[#F1A501] ease-in-out duration-200'
+          className='border border-dark-blue rounded-md py-2 px-5 ml-7 lg:ml-14 hover:bg-[#F1A501]  focus:bg-[#F1A501] ease-in-out duration-200'
           href={''}
           aria-label='Sign up'
         >
           Sign up
         </Link>
-        <Link className='flex gap-1 ml-14' href={''} aria-label='toggle lang'>
+        <Link
+          className='flex gap-1 ml-7 lg:ml-14'
+          href={''}
+          aria-label='toggle lang'
+        >
           EN
           <ArrowToBottom className={'h-4 w-4 stroke-black'} />
         </Link>
