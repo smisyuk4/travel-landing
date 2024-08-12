bbf9d10a233767503e60c69f1bd8e6cff88b51f0
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-13 00:40:01 +0300

    write styles for category section

--- components/Category.tsx
+++ components/Category.tsx
@@ -3,21 +3,33 @@ import Image from 'next/image';
 
 const Category = () => {
   return (
-    <section>
-      <h3>CATEGORY</h3>
-      <h2>We Offer Best Services</h2>
+    <section className='bg-[#FFFFFF] py-[60px]'>
+      <h3 className='mb-[10px] text-center text-[18px] font-semibold text-[#5E6282]'>
+        CATEGORY
+      </h3>
+      <h2 className='mb-[70px] text-center text-[50px] font-bold text-[#14183E]'>
+        We Offer Best Services
+      </h2>
 
-      <ul>
+      <ul className='flex justify-center gap-[90px]'>
         {CATEGORY.map(({ image, title, description }, idx) => (
-          <li key={idx}>
+          <li
+            key={idx}
+            className="hover:after:bottom-30 relative w-[190px] cursor-pointer bg-[#FFFFFF] p-[20px] shadow-slate-300 duration-200 ease-in-out hover:z-10 hover:rounded-[20px] hover:shadow-xl hover:after:absolute hover:after:-left-5 hover:after:-z-10 hover:after:block hover:after:h-[50px] hover:after:w-[50px] hover:after:rounded-br-[10px] hover:after:rounded-tl-[30px] hover:after:bg-[#DF6951] hover:after:content-[''] focus:rounded-[30px]"
+          >
             <Image
               src={image?.src}
               width={100}
               height={100}
               alt='icon category'
+              className='mx-auto mb-[30px] h-[100px] w-auto object-contain'
             />
-            <h4>{title}</h4>
-            <p>{description}</p>
+            <h4 className='mb-[15px] text-center text-[20px] text-[#1E1D4C]'>
+              {title}
+            </h4>
+            <p className='text-center text-[16px] text-[#5E6282]'>
+              {description}
+            </p>
           </li>
         ))}
       </ul>
