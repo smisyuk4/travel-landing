cb0ac16161ee538eb85189e28cc6a194150b786c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-14 00:23:01 +0300

    write some styles for destination section

--- components/Destinations.tsx
+++ components/Destinations.tsx
@@ -15,23 +15,31 @@ const Destinations = () => {
         {DESTINATIONS.map(({ image, destination, cost, duration }, idx) => (
           <li
             key={idx}
-            className='relative w-[190px] cursor-pointer bg-[#FFFFFF] p-[20px] shadow-slate-300
-              duration-200 ease-in-out hover:z-10 hover:rounded-[20px] hover:shadow-xl
-              focus:rounded-[30px]'
+            className='relative w-[314px] cursor-pointer bg-[#FFFFFF] pb-[20px] shadow-slate-300
+              duration-200 ease-in-out rounded-[20px] shadow-lg'
           >
             <Image
-              className='object-cover'
+              className='object-cover w-full rounded-[20px]'
               src={image.src}
               alt={destination}
-              height={200}
-              width={200}
+              height={400}
+              width={350}
             />
-            <h4>{destination}</h4>
-            <p>{`$${cost}`}</p>
-            <p>
-              <NavigationIcon className={'h-4 w-4 stroke-black'} />
-              {`${duration} Days Trip`}
-            </p>
+            <div
+              className='flex-col py-[17px] px-[20px] h-[100px] absolute bottom-[-15px] bg-[#FFFFFF]
+                w-full rounded-b-[20px]'
+            >
+              <div className='flex justify-between'>
+                <h4 className='text-[#5E6282] text-[18px] mb-[15px] font-bold'>
+                  {destination}
+                </h4>
+                <p className='text-[#5E6282] text-[18px]'>{`$ ${cost}`}</p>
+              </div>
+              <p className='flex gap-[8px] items-center'>
+                <NavigationIcon className={'h-4 w-4 stroke-black'} />
+                {`${duration} Days Trip`}
+              </p>
+            </div>
           </li>
         ))}
       </ul>
