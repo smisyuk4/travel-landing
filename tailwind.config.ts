e505da05fafb7e2ba93542ffafccfbbaa28dc791
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-14 23:51:05 +0300

    write colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,17 @@ const config: Config = {
         //'header-decore': "url('../public/header-decore.svg')",
       },
       colors: {
-        'dark-blue': '#212832',
-        'brand-red': '#DF6951',
+        accent: '#52a3b1',
+        second: '#93d1a4',
+        'custom-bg-color': {
+          100: '#e9e5cb',
+          200: '#d1ddb9',
+        },
+        'custom-text-color': {
+          100: '#FFFFFF',
+          400: '#4b414b',
+          800: '#000000',
+        },
       },
     },
   },
