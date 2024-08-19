79780c12e7dcb57dd2630720ee550164d7f65416
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-20 00:24:25 +0300

    write subscribe form

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -1,21 +1,34 @@
+'use client';
+import { useState } from 'react';
 import { EmailIcon } from '@/assets/icons';
 
 const SubscribeForm = () => {
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <div className='bg-custom-bg-color-200 h-[300px] w-[900px] rounded-tl-[100px] mx-auto p-14'>
       <h3 className='text-2xl font-bold mb-10 text-center text-custom-text-color-400'>
         Subscribe to get information, latest news and other interesting offers
         about Jadoo
       </h3>
-      {/*focus-within:hidden*/}
+
       <form className='flex items-center justify-between'>
         <label className='w-[60%] relative'>
-          <EmailIcon className='absolute top-3 left-0 flex items-center pl-2 h-6 w-8' />
+          {inputValue === '' && (
+            <EmailIcon
+              className='absolute top-3 left-1 flex items-center pl-2 h-6 w-8
+                stroke-custom-text-color-400'
+            />
+          )}
+
           <input
-            className='w-full rounded-md px-5 py-2 text-lg bg-custom-bg-color-100'
+            className='w-full rounded-md px-10 py-2 text-lg bg-custom-bg-color-100
+              placeholder-custom-text-color-400'
             id='email'
             placeholder='Your email'
             type='email'
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
           />
         </label>
 
