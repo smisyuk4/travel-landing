5079604388d7d065d6b5958881a5f1bd1ecc7a8c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-19 23:59:24 +0300

    clear

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -7,27 +7,17 @@ const SubscribeForm = () => {
         Subscribe to get information, latest news and other interesting offers
         about Jadoo
       </h3>
-
+      {/*focus-within:hidden*/}
       <form className='flex items-center justify-between'>
-        <label className='w-[60%] relative focus-within:text-red-600'>
-          <EmailIcon className='absolute inset-y-0 left-0 flex items-center pl-2 h-5 w-5' />
+        <label className='w-[60%] relative'>
+          <EmailIcon className='absolute top-3 left-0 flex items-center pl-2 h-6 w-8' />
           <input
-            className='w-full rounded-md px-5 py-2 text-lg bg-custom-bg-color-100 fontAwesome'
+            className='w-full rounded-md px-5 py-2 text-lg bg-custom-bg-color-100'
             id='email'
             placeholder='Your email'
             type='email'
           />
         </label>
-        {/*<label class='relative block'>
-          <input
-            className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border
-              border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none
-              focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-            placeholder='Search for anything...'
-            type='text'
-            name='search'
-          />
-        </label>*/}
 
         <button className='w-[35%] subscribe-button' aria-label='Submit'>
           Subscribe
