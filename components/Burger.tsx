7112adb8105976dda3b9b4b4309649c40c7461fe
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-20 22:52:21 +0300

    fix styles in burger menu

--- components/Burger.tsx
+++ components/Burger.tsx
@@ -63,29 +63,23 @@ const Burger = () => {
                   href={href}
                   label={title}
                   title={title}
-                  onClickFunc={() => setIsOpenMenu(prev => !prev)}
+                  onClick={() => setIsOpenMenu(prev => !prev)}
                 />
               </li>
             ))}
           </ul>
         </nav>
 
-        <Link
-          className='mt-6 rounded-md border border-dark-blue px-4 py-2 duration-200 ease-in-out
-            hover:bg-[#F1A501] focus:bg-[#F1A501]'
-          href={''}
-          aria-label='Sign up'
-        >
+        <Link className='mt-6 main-button' href={''} aria-label='Sign up'>
           Sign up
         </Link>
-        <Link className='mt-6 flex gap-1' href={''} aria-label='toggle lang'>
+        {/*<Link className='mt-6 flex gap-1' href={''} aria-label='toggle lang'>
           EN
           <ArrowToBottomIcon className={'h-4 w-4 stroke-black'} />
-        </Link>
+        </Link>*/}
       </div>
     </div>
   );
 };
 
 export default Burger;
-//isOpenMenu ? 'scale-10 translate-y-16' : 'scale-0 translate-y-0'
